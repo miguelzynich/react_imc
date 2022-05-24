b920a0bd4f8e83bf92fe42c23876009717c8f5de
Marcelo <marcelo.cavalcante@ebac.art.br>
2022-05-24 20:54:24 -0300

    calculadora imc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import IMC from './components/IMC';
+import CalculatorIMC from './components/IMC';
 
 function App() {
   return (
     <div className="App">
       <Header title="Título do APP" />
+      <CalculatorIMC />
     </div>
   );
 }
