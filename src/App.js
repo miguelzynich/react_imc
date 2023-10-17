bf22e1054656bad949519c71c7fd2ae04e1dcf04
miguelzynich@gmail.com <miguelzynich@gmail.com>
2023-10-17 11:39:27 -0300

    Login_adicionado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header';
 import CalculatorIMC from './components/IMC';
+import Login from './components/Login/LoginControl'
+
 
 function App() {
   return (
     <div className="App">
       <Header title="Título do APP" />
       <CalculatorIMC />
+
+    <Login name={'Miguel'} />
+
     </div>
   );
 }
